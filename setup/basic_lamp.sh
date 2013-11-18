12144972c160bb036143a71cf1c9352d50d8ba20
nandorocker <hello@nandorocker.com>
2013-11-18 20:12:01 +0000

    Resolving conflict insanity

--- setup/basic_lamp.sh
+++ setup/basic_lamp.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y vim curl git-core apache2 php5 php5-mysql php5-gd php5-c
 sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password password  '
 sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password_again password  '
 sudo apt-get -y install mysql-server
+sudo cp -f setup/apache_conf /etc/apache2/sites-available/
 sudo service apache2 restart
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-sudo cp -f /vagrant/setup/apache_conf /etc/apache2/sites-available/
 sudo apt-get -y install phpmyadmin
