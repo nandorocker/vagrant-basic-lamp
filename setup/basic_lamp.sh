c89d515a8c095ae164979a5fc0395ec0ae565e3a
Nando Rossi <hello@nandorocker.com>
2013-12-07 04:41:43 +0100

    Fixed terminal characters

--- setup/basic_lamp.sh
+++ setup/basic_lamp.sh
@@ -4,9 +4,8 @@ sudo apt-get upgrade
 sudo aptitude update
 sudo aptitude install build-essential
 sudo apt-get install -y vim curl git-core apache2 php5 php5-mysql php5-gd php5-curl
-sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password password  '
-sudo debconf-set-selections <<< 'mysql-server-<version> mysql-server/root_password_again password  '
-sudo apt-get -y install mysql-server
+sudo apt-get -qy install mysql-server
+echo -e \\033c # resets characters after mySQL sets everything crazy
 sudo cp -f setup/apache_conf /etc/apache2/sites-available/
 sudo service apache2 restart
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
